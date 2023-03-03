173e5bef8798792d26ca802ffcd42f231fa6829c
Anish Wagle <anish.wagle192@gmail.com>
2023-03-03 09:00:38 +0545

    program commit

---
+++ Class2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Class2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string,bool, int, float, list, dic,char
+            var list = new List<string>() { "Ram","Sita"};
+            var dic = new Dictionary<string, int>();
+            dic["age"] = 15;
+            //Console.Write("x:");
+            //var x = Console.ReadLine();
+            //Console.Write("y:");
+            //var y =Console.ReadLine();
+            //var z = x.ToString();
+            var name3 = $"{dic["Husband"]} and {dic["Wife"]} are husband and wife";
+            Console.WriteLine(name3);
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
